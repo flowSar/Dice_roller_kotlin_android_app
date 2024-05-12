98e6b3742738bf9bc12e12baf506ed4ef2b11b01
flowsar <brahim.sar151@gmail.com>
2024-05-12 19:23:09 +0100

    optimize code by view binding

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,27 +1,27 @@
 package com.example.diceroller
 
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageView
-import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.databinding.DataBindingUtil
+import com.example.diceroller.databinding.ActivityMainBinding
 import kotlin.random.Random
 
 class MainActivity : ComponentActivity()
 {
+    private lateinit var binding: ActivityMainBinding
     private var randomInt = 0
     lateinit var diceImage: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        diceImage = findViewById<ImageView>(R.id.dice_img)
-        val roll_button: Button = findViewById<Button>(R.id.roll_btn)
-        roll_button.text = "roll Dice"
-        roll_button.setOnClickListener {
-            rollDice()
-            Toast.makeText(this, "${randomInt}", Toast.LENGTH_SHORT).show()
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.apply {
+            diceImage = diceImg
+            rollBtn.text = "roll Dice"
+            rollBtn.setOnClickListener{
+                rollDice()
+            }
         }
-
     }
 
     private fun rollDice() {
